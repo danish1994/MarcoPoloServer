8a2da4efaee0d7c877af4f5c8da7f5162dfab9fe
Danish <danish5095@gmail.com>
2020-05-23 19:50:30 +0530

    Updated Caching

--- service.js
+++ service.js
@@ -2,22 +2,26 @@ const indexCache = {};
 const numberCache = {};
 
 const getIndexMapping = (i) => {
-    if (!indexCache[i]) {
-        if (i % 7 === 0 && i % 4 === 0) {
-            indexCache[i] = 'marcopolo';
-        }
-
-        if (i % 4 === 0) {
-            indexCache[i] = 'marco';
-        }
-
-        if (i % 7 === 0) {
-            indexCache[i] = 'polo';
-        }
-
-        indexCache[i] = i.toString();
+    if (i % 7 === 0 && i % 4 === 0) {
+        return 'marcopolo';
     }
 
+    if (i % 4 === 0) {
+        return 'marco';
+    }
+
+    if (i % 7 === 0) {
+        return 'polo';
+    }
+
+    return i.toString();
+
+};
+
+const processInputByIndex = (i) => {
+    if (!indexCache[i]) {
+        indexCache[i] = getIndexMapping(i);
+    }
     return indexCache[i];
 };
 
@@ -26,7 +30,7 @@ const processOutput = (n) => {
         let result = '';
         for (let i = 1; i <= n; i++) {
             // Adding new line after 1000 numbers
-            result += (getIndexMapping(i) + ((i % 1000 === 0) ? '\n' : ' '));
+            result += (processInputByIndex(i) + ((i % 1000 === 0) ? '\n' : ' '));
         }
 
         numberCache[n] = result;
